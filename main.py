fd3dd6a45bc89894781bc7dfc78aef8137b6f6d2
Rostyslav Derkach <rostyslavderkach05@gmail.com>
2025-02-16 14:39:31 +0200

    pacman initialized

--- main.py
+++ main.py
@@ -1,6 +1,41 @@
-def main():
-    print("Hello from uni-cicd-game!")
+import pygame
+from pygame.locals import *
+from constants import *
+from pacman.pacman import Pacman
 
+class GameController(object):
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode(SCREENSIZE, 0, 32)
+        self.clock = pygame.time.Clock()
+        self.background = None
+
+    def setBackground(self):
+        self.background = pygame.surface.Surface(SCREENSIZE).convert()
+        self.background.fill(BLACK)
+
+    def startGame(self):
+        self.setBackground()
+        self.pacman = Pacman()
+
+    def update(self):
+        dt = self.clock.tick(30) / 1000.0
+        self.pacman.update(dt)
+        self.checkEvents()
+        self.render()
+
+    def checkEvents(self):
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                exit()
+
+    def render(self):
+        self.screen.blit(self.background, (0, 0))
+        self.pacman.render(self.screen)
+        pygame.display.update()
 
 if __name__ == "__main__":
-    main()
+    game = GameController()
+    game.startGame()
+    while True:
+        game.update()
