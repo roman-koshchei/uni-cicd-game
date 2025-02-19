7f4d0f27f733292770b5a31db0a98f435031fef2
Roman Koshchei <romankoshchei@gmail.com>
2025-02-19 16:09:09 +0200

    more ghosts

--- ghosts/ghost.py
+++ ghosts/ghost.py
@@ -16,6 +16,7 @@ class Ghost(Entity):
         pacman: Pacman | None = None,
         sprite_manager: SpriteManager | None = None,
         ghost_type="red",
+        blinky = None,
     ):
         Entity.__init__(self, node)
         self.name = GHOST
@@ -29,6 +30,9 @@ class Ghost(Entity):
         self.animation_frame = 0
         self.animation_speed = 0.2  # seconds per frame
         self.animation_timer = 0
+
+        self.blinky = blinky
+        self.home_node = node
 
     def update(self, dt):
         self.mode.update(dt)
@@ -120,3 +124,114 @@ class Ghost(Entity):
         elif self.direction == RIGHT:
             return "right"
         return "right"  # Default direction when stopped
+
+
+class Blinky(Ghost):
+    def __init__(self, node, pacman=None, blinky=None):
+        Ghost.__init__(self, node, pacman, blinky=blinky)
+        self.name = BLINKY
+        self.color = RED
+
+
+class Pinky(Ghost):
+    def __init__(self, node, pacman=None, blinky=None):
+        Ghost.__init__(self, node, pacman, blinky=blinky)
+        self.name = PINKY
+        self.color = PINK
+
+    def scatter(self):
+        self.goal = Vector2(TILEWIDTH * NCOLS, 0)
+
+    def chase(self):
+        self.goal = (
+            self.pacman.position
+            + self.pacman.directions[self.pacman.direction] * TILEWIDTH * 4
+        )
+
+
+class Inky(Ghost):
+    def __init__(self, node, pacman=None, blinky=None):
+        Ghost.__init__(self, node, pacman, blinky=blinky)
+        self.name = INKY
+        self.color = TEAL
+
+    def scatter(self):
+        self.goal = Vector2(TILEWIDTH * NCOLS, TILEHEIGHT * NROWS)
+
+    def chase(self):
+        vec1 = (
+            self.pacman.position
+            + self.pacman.directions[self.pacman.direction] * TILEWIDTH * 2
+        )
+        vec2 = (vec1 - self.blinky.position) * 2
+        self.goal = self.blinky.position + vec2
+
+
+class Clyde(Ghost):
+    def __init__(self, node, pacman=None, blinky=None):
+        Ghost.__init__(self, node, pacman, blinky=blinky)
+        self.name = CLYDE
+        self.color = ORANGE
+
+    def scatter(self):
+        self.goal = Vector2(0, TILEHEIGHT * NROWS)
+
+    def chase(self):
+        d = self.pacman.position - self.position
+        ds = d.magnitude_squared()
+        if ds <= (TILEWIDTH * 8) ** 2:
+            self.scatter()
+        else:
+            self.goal = (
+                self.pacman.position
+                + self.pacman.directions[self.pacman.direction] * TILEWIDTH * 4
+            )
+
+
+class GhostGroup(object):
+    def __init__(self, node, pacman):
+        self.blinky = Blinky(node, pacman)
+        self.pinky = Pinky(node, pacman)
+        self.inky = Inky(node, pacman, self.blinky)
+        self.clyde = Clyde(node, pacman)
+        self.ghosts: list[Ghost] = [self.blinky, self.pinky, self.inky, self.clyde]
+
+    def __iter__(self):
+        return iter(self.ghosts)
+
+    def update(self, dt):
+        for ghost in self:
+            ghost.update(dt)
+
+    def start_freight(self):
+        for ghost in self:
+            ghost.start_freight()
+        self.reset_points()
+
+    def set_spawn_node(self, node):
+        for ghost in self:
+            ghost.set_spawn_node(node)
+
+    def update_points(self):
+        for ghost in self:
+            ghost.points *= 2
+
+    def reset_points(self):
+        for ghost in self:
+            ghost.points = 200
+
+    def reset(self):
+        for ghost in self:
+            ghost.reset()
+
+    def hide(self):
+        for ghost in self:
+            ghost.visible = False
+
+    def show(self):
+        for ghost in self:
+            ghost.visible = True
+
+    def render(self, screen):
+        for ghost in self:
+            ghost.render(screen)
